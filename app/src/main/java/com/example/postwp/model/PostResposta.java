9565b5b26e62ecdd29159db4529dc08fbd5ff804
Jonathan Reis <jonathanreis7834@gmail.com>
2019-04-24 12:31:37 -0300

    Criada a classe de resposta pra receber o array de posts.

---
+++ app/src/main/java/com/example/postwp/model/PostResposta.java
@@ -0,0 +1,16 @@
+package com.example.postwp.model;
+
+import java.util.ArrayList;
+
+public class PostResposta {
+
+    private ArrayList<Post> results;
+
+    public ArrayList<Post> getResults() {
+        return results;
+    }
+
+    public void setResults(ArrayList<Post> results) {
+        this.results = results;
+    }
+}
