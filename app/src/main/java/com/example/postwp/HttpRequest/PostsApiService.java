25cc7de8f7044a4db9cdd012a0f8fe329b4f5d06
Jonathan Reis <jonathanreis7834@gmail.com>
2019-04-25 15:47:45 -0300

    Interface editada para receber <List<Post>>.

--- app/src/main/java/com/example/postwp/HttpRequest/PostsApiService.java
+++ app/src/main/java/com/example/postwp/HttpRequest/PostsApiService.java
@@ -1,6 +1,8 @@
 package com.example.postwp.HttpRequest;
 
-import com.example.postwp.model.PostResposta;
+import com.example.postwp.model.Post.Post;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -8,5 +10,5 @@ import retrofit2.http.GET;
 public interface PostsApiService {
 
     @GET("wp-json/wp/v2/posts")
-    Call<PostResposta> listaPosts();
+    Call <List<Post>> listaPosts();
 }
