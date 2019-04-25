f92e1a20c2d19529dede586db3a974e657e48f20
Jonathan Reis <jonathanreis7834@gmail.com>
2019-04-25 15:42:27 -0300

    Update da chamada do adapter que agora traz o metodo adicionarListaPost do PostAdapter.

--- app/src/main/java/com/example/postwp/activity/MainActivity.java
+++ app/src/main/java/com/example/postwp/activity/MainActivity.java
@@ -7,22 +7,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.postwp.HttpRequest.PostsApiService;
 import com.example.postwp.R;
 import com.example.postwp.adapter.PostsAdapter;
-import com.example.postwp.model.Post;
+import com.example.postwp.model.Post.Post;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerPosts;
-    private List<Post> listaPosts = new ArrayList<>();
-    private PostsAdapter adapter;
+    //private List<Post> listaPosts = new ArrayList<>();
+    private PostsAdapter listaPosts;
+    //private PostsAdapter adapter;
+
+    private Retrofit retrofit;
+
+    public List<Post> postResposta = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,16 +60,50 @@ public class MainActivity extends AppCompatActivity {
     private void inicializarComponentes() {
         recyclerPosts = findViewById(R.id.recyclerPosts);
 
-        adapter = new PostsAdapter( listaPosts, getApplicationContext()/*, usuario*/ );
+        listaPosts = new PostsAdapter( postResposta, getApplicationContext() );
+
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerPosts.setLayoutManager( layoutManager );
         recyclerPosts.setHasFixedSize(true);
-        recyclerPosts.setAdapter( adapter );
+        recyclerPosts.setAdapter( listaPosts );
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://demo.wp-api.org")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
         listPosts();
+
+        //Log.d("Depurador2 ", " Retorno2: ");
     }
 
     private void listPosts() {
+
+        PostsApiService service = retrofit.create(PostsApiService.class);
+        Call <List<Post>> PostRespostaCall = service.listaPosts();
+
+        PostRespostaCall.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (response.isSuccessful()) {
+
+                    postResposta = response.body();
+
+                    listaPosts.adicionarListaPost(postResposta);
+
+                    Log.d("Depurador ", " Retornou: " + postResposta );
+
+                } else {
+                    Log.e("Depurador", " onResponse: " + response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.e("Depurador", " onFailure: " + t.getMessage());
+            }
+        });
 
     }
 
