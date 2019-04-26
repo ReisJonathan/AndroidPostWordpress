e9f3cd451c70ffbc5e8b0fb21c7ebe54d9e09e2c
Jonathan Reis <jonathanreis7834@gmail.com>
2019-04-25 21:43:04 -0300

    Adicionada o GET media/{id} na interface

--- app/src/main/java/com/example/postwp/HttpRequest/PostsApiService.java
+++ app/src/main/java/com/example/postwp/HttpRequest/PostsApiService.java
@@ -1,14 +1,21 @@
 package com.example.postwp.HttpRequest;
 
+import com.example.postwp.model.Media.Media;
 import com.example.postwp.model.Post.Post;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface PostsApiService {
 
     @GET("wp-json/wp/v2/posts")
     Call <List<Post>> listaPosts();
+
+    @GET("wp-json/wp/v2/media/{id}")
+    Call <Media> retornaMedia(@Path("id") int id );
 }
